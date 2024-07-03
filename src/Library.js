23cfb02549ce15d0397fe9d14037f99b2c1feefa
soconzan <bin00inbubble@gmail.com>
2024-07-03 15:05:46 +0900

    날짜 수정

--- src/Library.js
+++ src/Library.js
@@ -23,7 +23,7 @@ import wiger5 from './assets/images/wiger_5.png';
 import iconSearch from './assets/images/icon-search.png';
 
 const wigerImages = [wiger1, wiger2, wiger3, wiger4, wiger5];
-const dateStrings = ["7/8 ~ 7/14", "7/15 ~ 7/21", "7/22 ~ 7/28", "7/29 ~ 8/4", "8/5 ~ 8/16"]
+const dateStrings = ["7/8 ~ 7/14", "7/15 ~ 7/21", "7/22 ~ 7/28", "7/29 ~ 8/4", "8/5 ~ 8/18"]
 
 const Library = () => {
     const [userId, setUserId] = useState('');
